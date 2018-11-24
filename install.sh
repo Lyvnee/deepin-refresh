910873ad499ca366b54f6b00c6eabde650fd2689
Lyvnee <me@lyvnee.com>
2018-11-24 19:16:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+echo "==========Start!=========="
+dirname="$HOME/.config/deepin/dde-file-manager/menuextensions"
+if [ ! -d  $dirname ];then
+  mkdir -p $dirname
+else
+  echo "Menuextensions dir exist!"
+fi
+cp $(cd `dirname $0`; pwd)/refresh.json $dirname/refresh.json
+sudo cp $(cd `dirname $0`; pwd)/deepin-refresh /usr/bin/deepin-refresh
+sudo chmod 755 /usr/bin/deepin-refresh
 sudo apt install xautomation
+echo "==========Finished!=========="
