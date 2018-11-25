306d04d1da1544f1f61c21078643611ea9f52c4d
Lyvnee <me@lyvnee.com>
2018-11-25 10:46:12 +0800

    add -y to apt

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ fi
 cp $(cd `dirname $0`; pwd)/refresh.json $dirname/refresh.json
 sudo cp $(cd `dirname $0`; pwd)/deepin-refresh /usr/bin/deepin-refresh
 sudo chmod 755 /usr/bin/deepin-refresh
-sudo apt install xautomation
+sudo apt install xautomation -y
 echo "==========Finished!=========="
